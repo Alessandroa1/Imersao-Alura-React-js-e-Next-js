77de712448dbe7c12a50428fb4b66f28020bf972
Alessandroa1 <54918597+Alessandroa1@users.noreply.github.com>
2021-02-02 10:54:53 -0300

    alteraçõe finais

--- rockquiz/pages/index.js
+++ rockquiz/pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 //const BackgroundImage = styled.div`
 //  background-image: url(${db.bg});
@@ -26,15 +30,41 @@ export const QuizContainer = styled.div`
 
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>RockQuiz</title>          
+        
+      </Head>
       <QuizContainer>
         <Widget>
         <Widget.Header>
-          <h1>This is Rock'Roll Baby!</h1> 
+          <h1>This is Rock n'Roll Baby!</h1> 
             </Widget.Header>
-          <Widget.Content>         
-         <p>loren ipsum</p>  
+          <Widget.Content>
+            <p>{db.description}</p>
+          <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <Input
+                name="nome do usuário"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}               
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}                
+              </Button>
+            </form> 
+                      
+         
           </Widget.Content>         
         </Widget>   
         <Widget>
